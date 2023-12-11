58502a98813670b7b8e112cafa25b2bb03caf80c
rifatjahan823 <rifatjahan823@gmail.com>
2023-12-11 13:54:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7b7ma7m.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+    try {
+      await client.connect();
+      const db = client.db('products');
+      const productCollection = db.collection('product');
+  
+      app.get('/category', async (req, res) => {
+        const cursor = productCollection.find({});
+        const products = await cursor.toArray();
+        res.send({ products });
+      });
+      
+    } finally {
+      // Make sure to close the connection when done
+    //   await client.close();
+    }
+  };
+  
+
+run().catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
